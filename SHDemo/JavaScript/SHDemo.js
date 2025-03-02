4a8282dba0c9d2a20a0f32706722c4d66ab7383b
theThousandSonsArchivalProject <152043721+theThousandSonsArchivalProject@users.noreply.github.com>
2025-03-01 21:53:44 -0500

    add hidden text behaviour - SHDemo.js

--- SHDemo/JavaScript/SHDemo.js
+++ SHDemo/JavaScript/SHDemo.js
@@ -1,1 +1,20 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+const form = document.getElementById('form');
+const msgInput = document.getElementById('msgInput');
+const hidden = document.getElementById("hidden");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+	// console.log("submittion received");
+  
+  // check if input matches expected
+  if (msgInput.value != "ATL"){
+  	//console.log("wrong input");
+    alert("Not really... Try again");
+  	location.reload();
+  } else {
+    // console.log("right input")
+  	hidden.style.opacity = 1;
+  }
+});
