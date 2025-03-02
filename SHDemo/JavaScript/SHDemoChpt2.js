9deff277e011a3e47e9bc719227a967759ef0c23
theThousandSonsArchivalProject <152043721+theThousandSonsArchivalProject@users.noreply.github.com>
2025-03-01 22:20:13 -0500

    Create SHDemoChpt2.js

---
+++ SHDemo/JavaScript/SHDemoChpt2.js
@@ -0,0 +1,19 @@
+document.getElementById('date').innerHTML = new Date().toDateString();
+
+const form = document.getElementById('form');
+const msgInput = document.getElementById('msgInput');
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+	console.log("submittion received");
+  
+  // check if input matches expected
+  if (msgInput.value != "DBOA"){
+  	console.log("wrong input");
+    alert("Not really... Try again");
+  	location.reload();
+  } else {
+    console.log("right input")
+  	// hidden.style.opacity = 1;
+  }
+});
